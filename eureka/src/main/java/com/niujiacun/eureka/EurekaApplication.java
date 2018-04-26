6e40a47f80d386d99559a75a79bfc76232112064
wuhaisheng <wuhaisheng@binggou.com>
2018-04-26 14:03:35 +0800

    框架搭建

---
+++ eureka/src/main/java/com/niujiacun/eureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.niujiacun.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 武海升
+ * @version 2.0
+ * @description
+ * @date 2018-04-26 13:23
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
